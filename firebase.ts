dee7654d963af006b0eb6c3e763614470ea1a0de
Võ Trường <truong.vd2000@gmail.com>
2025-02-04 10:19:52 +0700

    Update db

--- firebase.ts
+++ firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_DEV_API_KEY,
   authDomain: import.meta.env.VITE_DEV_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_DEV_DATABASE_URL,
-  projectId: import.meta.env.VITE_DEV_PROJECT_ID,
+//  projectId: import.meta.env.VITE_DEV_PROJECT_ID,
   storageBucket: import.meta.env.VITE_DEV_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_DEV_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_DEV_APP_ID,
